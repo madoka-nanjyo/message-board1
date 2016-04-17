a3aaa7a34cc031d5de030d23f37f41d25725f056
madoka nanjyo <madoka-nanjyo@united.jp>
2016-04-17 23:00:57 +0000

    reiauto

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160411165855) do
+ActiveRecord::Schema.define(version: 20160417141214) do
 
   create_table "messages", force: :cascade do |t|
     t.string   "name"
